39b07f5b22e6d253e51fe3c3deeaa5439185026c
subair <subair@crudx.tech>
2025-03-24 11:45:20 +0530

    fixed railway

--- config/db.js
+++ config/db.js
@@ -10,7 +10,11 @@ const db = knex({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    ssl: { rejectUnauthorized: false }, // Accept self-signed certificates
   },
+  pool: { min: 2, max: 10 }, // Helps manage connections efficiently
+  acquireConnectionTimeout: 10000, // 10 seconds timeout to avoid ETIMEDOUT
 });
 
 export default db;
